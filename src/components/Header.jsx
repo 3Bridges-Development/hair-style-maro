950e8675e9c9028643d39815179cdeaaa8a6ce2d
nkleinmann <nekleinmann@gmail.com>
2024-04-01 20:23:21 -0400

    Import google font

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
     return (
-        <h1> HII I d</h1>
+        <h1>Haera's HairCare</h1>
     )
 }
 
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -6,7 +6,7 @@ import Contact from "../components/Contact";
 
 function Home() {
         return (
-                <div>
+                <div className="home">
                         <Header />
                         <ImageGallery />
                         <Contact />
